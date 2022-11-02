1f0067d4658b13bad09ab03beb7d411afcd4d1e2
Nicholas Richmond <114439546+all4nitrous@users.noreply.github.com>
2022-11-02 02:04:40 -0500

    Finished header, now making responsive

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
-const  = () => {
+import {FaGithub} from 'react-icons/fa'
+import {BsInstagram} from 'react-icons/bs'
+const  HeaderSocials= () => {
   return (
     <div className='header_socials'>
-        <a href="https://linkedin.com/in/nicholas-richmond" target= "_blank"><BsLinkedin /></a>
-        <a href="https://github.com/all4nitrous" target= "_blank"></a>
-        <a href="https://www.instagram.com/all4nitrous_WebDev/" target= "_blank"></a>
+        <a href="https://linkedin.com/in/nicholas-richmond" target= "_blank"><BsLinkedin/></a>
+        <a href="https://github.com/all4nitrous" target= "_blank"><FaGithub/></a>
+        <a href="https://www.instagram.com/all4nitrous_WebDev/" target= "_blank"><BsInstagram/></a>
     </div>
   )
 }
 
-export default 
+export default HeaderSocials
